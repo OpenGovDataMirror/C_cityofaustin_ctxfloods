52fe59b98638953860d08613ec88b3af38e393ac
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-14 10:21:17 +0200

    init

---
+++ src/components/Shared/Header/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import CtxFloodsLogoDarkSvg from 'images/ctx-floods-logo-dark.svg';
+import './Header.css';
+
+export default class Header extends Component {
+  static propTypes = {
+    location: PropTypes.object.isRequired,
+    title: PropTypes.string,
+    children: PropTypes.node.isRequired,
+  };
+
+  componentDidMount() {
+    document.title = `Central Texas Floods - Turn Around, Don't Drown`;
+  }
+
+  render() {
+    return (
+      <div className="Header">
+        <div className="Header__main">
+          <h1 className="Header__logo">
+            <img
+              src={CtxFloodsLogoDarkSvg}
+              alt="CTXfloods | Central Texas Floods"
+            />
+            <div className="Header__title">{this.props.title}</div>
+          </h1>
+          <ul className="Header__tabs">{this.props.children}</ul>
+        </div>
+      </div>
+    );
+  }
+}
